fcd97b25f986bc6c7e6e28ff6580c3d5f3154f8a
Jean Decian <jeandecian@hotmail.fr>
2025-04-21 21:55:48 -0400

    Initial Commit

---
+++ APIkintoo/APIkintooApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIkintooApp.swift
+//  APIkintoo
+//
+//  Created by Jean Decian on 2025-04-21.
+//
+
+import SwiftUI
+
+@main
+struct APIkintooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIkintoo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APIkintoo
+//
+//  Created by Jean Decian on 2025-04-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
